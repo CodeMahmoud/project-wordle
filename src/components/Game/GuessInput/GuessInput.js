8f5c107ef7e5171bc2d6b68d0d3a73708762307b
CodeMahmoud <mahmoudcode913@gmail.com>
2024-06-02 19:53:22 -0500

    added guess input

---
+++ src/components/Game/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({guess});
+    setGuess('');
+  }
+
+  return <>
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input 
+      required
+      maxLength={5}
+      minLength={5}
+      value={guess}
+      onChange={(event) => {
+        const nextGuess = event.target.value.toUpperCase();
+        setGuess(nextGuess)
+      }}
+      id="guess-input" type="text" />
+    </form>
+  </>;
+}
+
+export default GuessInput;
